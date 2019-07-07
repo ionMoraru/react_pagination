e38f24f6cd260c851bdcb6d0b2bc255d11043c67
ionMoraru <morarou.y@gmail.com>
2019-07-07 23:19:08 +0200

    feat(pagination): create pagination for 100 posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetsPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+    fetsPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Set current page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <button type="button" class="btn btn-light mb-5" onClick={() => setPostsPerPage(postsPerPage === 10 ? 5 : 10 )}>{`${postsPerPage === 10 ? 5 : 10} Posts per Page`}</button>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Posts = ({loading, posts}) => {
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    return (
+        <ul className="list-group mb-4">
+            {posts.map(post => {
+                return <li key={post.id} className="list-group-item">{post.title}</li>
+            })}
+        </ul>
+    )
+}
+export default Posts
