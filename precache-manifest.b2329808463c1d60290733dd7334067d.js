bbb386b93469d3ae002964fd1902c15025f289ab
ionMoraru <morarou.y@gmail.com>
2019-07-07 23:31:09 +0200

    Updates

---
+++ precache-manifest.b2329808463c1d60290733dd7334067d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8b788e9b42d85e5569a04e52bad557",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "9c6ab898f8907f8bef95",
+    "url": "/react_pagination/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "4dea63ec98f4a4958f92",
+    "url": "/react_pagination/static/js/2.02cc656a.chunk.js"
+  },
+  {
+    "revision": "9c6ab898f8907f8bef95",
+    "url": "/react_pagination/static/js/main.6fd4231d.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
