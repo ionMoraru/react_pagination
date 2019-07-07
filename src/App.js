928a190ef028f67a9d83728b7f9fcc168717b29a
ionMoraru <morarou.y@gmail.com>
2019-07-07 23:23:22 +0200

    feat(format): prettier formate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import Posts from './components/Posts';
-import Pagination from './components/Pagination';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -13,10 +13,10 @@ function App() {
   useEffect(() => {
     const fetsPosts = async () => {
       setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
       setPosts(res.data);
       setLoading(false);
-    }
+    };
     fetsPosts();
   }, []);
 
@@ -26,14 +26,22 @@ function App() {
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   // Set current page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <button type="button" class="btn btn-light mb-5" onClick={() => setPostsPerPage(postsPerPage === 10 ? 5 : 10 )}>{`${postsPerPage === 10 ? 5 : 10} Posts per Page`}</button>
+      <button
+        type="button"
+        class="btn btn-light mb-5"
+        onClick={() => setPostsPerPage(postsPerPage === 10 ? 5 : 10)}
+      >{`${postsPerPage === 10 ? 5 : 10} Posts per Page`}</button>
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
